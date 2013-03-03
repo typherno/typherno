9fd303c4e02e4e248e0b6b23a0ee9852c97e117e
Nate Diller <nate.diller@gmail.com>
2013-03-02 19:36:08 -0500

    Initial release, with Eclipse Public License banner

--- joinfs_helper.py
+++ joinfs_helper.py
@@ -1,7 +1,19 @@
-# Copyright 2012-2013 Nate Diller
-# All rights reserved
-
-# not for distribution
+#
+# Copyright (c) 2012, 2013 Nate Diller.
+# All rights reserved.
+# This component and the accompanying materials are made available
+# under the terms of the "Eclipse Public License v1.0"
+# which accompanies this distribution, and is available
+# at the URL "http://www.eclipse.org/legal/epl-v10.html".
+#
+# Initial Contributors:
+# Nate Diller - Initial contribution.
+#
+# Contributors:
+# 
+# Description:
+#
+#
 
 import os
 import sys
