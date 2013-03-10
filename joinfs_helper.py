069d034b639a92ba64ddc29eae2c24acd63b8df8
Nate Diller <nate.diller@gmail.com>
2013-03-10 08:57:41 -0400

    Add disk autodetect & auto-mkdisk to joinfs_helper, remove mkdisk

--- joinfs_helper.py
+++ joinfs_helper.py
@@ -17,6 +17,7 @@
 
 import os
 import sys
+import uuid
 
 from typherno_common import page_opener
 
@@ -39,14 +40,33 @@ def new_accumulators(disk, tracker):
 				yield "%s:9825" % tracker
 
 
+def alldisks(path):
+	for f in os.listdir(path):
+		if f.startswith("disk-") and os.path.isdir(path):
+			yield f
+
+
 host, path = sys.argv[1:3]
 if host == "localhost":
 	host = "127.0.0.1"
 path = os.path.abspath(path)
 name = os.path.basename(path)
 if not name.startswith("disk-"):
-	sys.stderr.write("No disk found at %s" % path)
-	sys.exit()
+	disks = [d for d in alldisks(path)]
+	if len(disks) > 1:
+		sys.stderr.write("Ambiguous disk path, multiple candidates found:\n")
+		for d in disks:
+			sys.stderr.write("  %s\n" % d)
+		sys.exit()
+	if disks:
+		name = disks[0]
+		path = os.path.join(path, name)
+	else:
+		name = "disk-%s" % uuid.uuid4()
+		path = os.path.join(path, name)
+		sys.stderr.write("Creating disk %s\n" % path)
+		os.mkdir(path)
+		os.mkdir(os.path.join(path, "logs"))
 
 sys.stdout.write(''.join(["%s %s\n" % (ar, path) for ar in new_accumulators(name[5:], host)]))
 
